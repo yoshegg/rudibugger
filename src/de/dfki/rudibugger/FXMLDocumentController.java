8bb8fa509df27158be73cb77cef1f0b4f162979b
Christophe Biwer <christophe.biwer@dfki.de>
2017-08-24 22:56:04 +0200

    added functioning TabPane (New Tabs, Closing)

--- src/de/dfki/rudibugger/FXMLDocumentController.java
+++ src/de/dfki/rudibugger/FXMLDocumentController.java
@@ -11,6 +11,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -24,6 +29,11 @@ public class FXMLDocumentController implements Initializable {
   private Label statusbar;
   
   @FXML
+  private TabPane tabpanex;
+  
+
+  
+  @FXML
   private void handleButtonAction(ActionEvent event) {
     System.out.println("You clicked me!");
     if (testing) {
@@ -34,10 +44,29 @@ public class FXMLDocumentController implements Initializable {
       testing = true;
     }
   }
+
+  @FXML
+  private void openNewTab(ActionEvent event) {
+    System.out.println("new Tab!");
+    Tab tab = new Tab("Tab " + (tabpanex.getTabs().size() + 1));
+    tabpanex.getTabs().add(tab);
+    tabpanex.getSelectionModel().select(tab);
+  }
+  
+  @FXML
+  private void tabClicked(MouseEvent event) {
+    System.out.println("tab clicked!");
+    
+    if (event.getButton() == MouseButton.MIDDLE) {
+      System.out.println("Middle mouse button clicked!");
+      Tab selectedTab = tabpanex.getSelectionModel().getSelectedItem();
+      System.out.println(selectedTab);
+    }
+  }
   
   @Override
   public void initialize(URL url, ResourceBundle rb) {
-    // TODO
+    tabpanex.setTabClosingPolicy(TabClosingPolicy.ALL_TABS);
   }  
   
 }
