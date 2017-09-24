192849c989aa17b814373fa47c2da55e9ea9f23d
Christophe Biwer <christophe.biwer@dfki.de>
2017-09-24 03:05:41 +0200

    added a file Icon to File imports in TreeView

--- src/main/java/de/dfki/rudibugger/ruleTreeView/ImportTreeItem.java
+++ src/main/java/de/dfki/rudibugger/ruleTreeView/ImportTreeItem.java
@@ -5,14 +5,24 @@
  */
 package de.dfki.rudibugger.ruleTreeView;
 
+import static de.dfki.rudibugger.ruleTreeView.BasicTreeItem.RULE_ICON_PATH;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 /**
  *
  * @author Christophe Biwer (yoshegg) christophe.biwer@dfki.de
  */
 public class ImportTreeItem extends BasicTreeItem {
 
+  static Image imgNever = new Image(RULE_ICON_PATH + "file.png");
+
+
   public ImportTreeItem(String importName, BasicTreeItem parent) {
     super(importName, parent);
+    ImageView folderIcon = new ImageView();
+    folderIcon.setImage(imgNever);
+    _hb.getChildren().add(0, folderIcon);
   }
 
   public ImportTreeItem(String importName) {
