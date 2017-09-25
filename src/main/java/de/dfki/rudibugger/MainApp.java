3a796790738b98f23f3688119345f4287aa9d0ca
Christophe Biwer <christophe.biwer@dfki.de>
2017-09-25 12:39:03 +0200

    [clean] MainApp.java

--- src/main/java/de/dfki/rudibugger/MainApp.java
+++ src/main/java/de/dfki/rudibugger/MainApp.java
@@ -7,10 +7,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.yaml.snakeyaml.Yaml;
 
 /**
  *
@@ -18,31 +18,33 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class MainApp extends Application {
 
+  /* the logger */
   static Logger log = Logger.getLogger("rudiLog");
+
+  /* a YAML instance for further use of YAML */
+  public Yaml yaml;
 
   @Override
   public void start(Stage stage) throws Exception {
 
-    // initialize log4j
+    /* initialize log4j */
     BasicConfigurator.configure();
     PropertyConfigurator.configure("src/main/resources/log4j/log4j.properties");
     log.info("Starting Rudibugger");
 
-    // initialize FXMLController
+    /* initialize YAML */
+    yaml = new Yaml();
+
+    /* initialize FXMLController */
     final Model model = new Model();
     FXMLLoader loader = new FXMLLoader();
     loader.setLocation(getClass().getResource("/fxml/Scene.fxml"));
-    loader.setControllerFactory(new Callback<Class<?>, Object>() {
-      @Override
-      public Object call(Class<?> aClass) {
-        return new FXMLController(model);
-      }
-    });
+    loader.setControllerFactory((Class<?> aClass) -> new FXMLController(model));
 
-    // bind stage to field
+    /* bind stage to field */
     model.stageX = stage;
 
-    // define GUI
+    /* define GUI */
     Parent root = (Parent) loader.load();
     Scene scene = new Scene(root);
     scene.getStylesheets().add("/styles/Styles.css");
@@ -51,7 +53,7 @@ public class MainApp extends Application {
     Image icon = new Image("file:src/main/resources/icons/baggerschaufel_titlebar_32x32.png");
     stage.getIcons().add(icon);
 
-    // show Rudibugger
+    /* show Rudibugger */
     stage.show();
     log.info("Rudibugger has been started");
   }
