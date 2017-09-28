5594fd76291e53f09cb42721d2d8fcbbda06acad
Christophe Biwer <christophe.biwer@dfki.de>
2017-09-28 15:51:57 +0200

    changed license header

--- src/main/java/de/dfki/rudibugger/Model.java
+++ src/main/java/de/dfki/rudibugger/Model.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Rudibugger is a debugger for .rudi code
+ * written in the context of a bachelor's thesis
+ * by Christophe Biwer (cbiwer@coli.uni-saarland.de)
  */
 package de.dfki.rudibugger;
 
