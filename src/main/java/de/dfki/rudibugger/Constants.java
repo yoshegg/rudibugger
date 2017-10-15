6872248b1c43a8ebef362bc8e52300988266fc46
Christophe Biwer <christophe.biwer@dfki.de>
2017-10-15 22:50:42 +0200

    defined some new Constants

--- src/main/java/de/dfki/rudibugger/Constants.java
+++ src/main/java/de/dfki/rudibugger/Constants.java
@@ -17,8 +17,17 @@ public class Constants {
   public static String RUN_FILE = "run.sh";
   public static String RULE_LOCATION_SUFFIX = "RuleLocation.yml";
 
-  public static Integer OVERWRITE_CHECK_CANCEL = 0;
-  public static Integer OVERWRITE_CHECK_CURRENT_WINDOW = 1;
-  public static Integer OVERWRITE_CHECK_NEW_WINDOW = 2;
+  public static final int OVERWRITE_CHECK_CANCEL = 0;
+  public static final int OVERWRITE_CHECK_CURRENT_WINDOW = 1;
+  public static final int OVERWRITE_CHECK_NEW_WINDOW = 2;
 
+  public static final int STATE_NEVER = 0;
+  public static final int STATE_IF_TRUE = 1;
+  public static final int STATE_IF_FALSE = 2;
+  public static final int STATE_ALWAYS = 3;
+  public static final int STATE_PARTLY = 9;
+
+  public static final int RULE_MODEL_UNCHANGED = 0;
+  public static final int RULE_MODEL_NEWLY_CREATED = 1;
+  public static final int RULE_MODEL_CHANGED = 2;
 }
