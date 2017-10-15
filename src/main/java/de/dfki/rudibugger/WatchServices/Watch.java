05813bfa18e18e777d45c61918b76a34c58480eb
Christophe Biwer <christophe.biwer@dfki.de>
2017-10-15 22:51:19 +0200

    [WIP] Commenting out old watch (some things are still needed)

--- src/main/java/de/dfki/rudibugger/WatchServices/Watch.java
+++ src/main/java/de/dfki/rudibugger/WatchServices/Watch.java
@@ -5,8 +5,6 @@
  */
 package de.dfki.rudibugger.WatchServices;
 
-import de.dfki.rudibugger.project.Project;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -24,129 +22,126 @@ import org.apache.log4j.Logger;
  * @author Christophe Biwer (yoshegg) christophe.biwer@dfki.de
  */
 public class Watch {
-
-  static Logger log = Logger.getLogger("rudiLog");
-
-  // the Project
-  private volatile Project _project;
-
-  // the Thread
-  private volatile Thread watchingTread;
-
-  public void startListening() {
-    log.info("Watch has been started.");
-    watchingTread = new Thread() {
-      public void run() {
-        try {
-          eventLoop();
-        } catch (IOException|InterruptedException ex) {
-          watchingTread = null;
-        }
-      }
-    };
-    watchingTread.setDaemon(true);
-    watchingTread.setName("watchingTread");
-    watchingTread.start();
-  }
-
-  public void shutDownListener() {
-    Thread thr = watchingTread;
-    if (thr != null) {
-      thr.interrupt();
-    }
-  }
-
-  private WatchService _watcherRootFolder;
-  private WatchService _watcherRudisFolder;
-
-
-  public void createProjectWatch(Project proj) {
-    _project = proj;
-    Path rootFolder = _project.getRootFolder();
-    Path rudisFolder = _project.getRudisFolder();
-    try {
-      _watcherRootFolder = FileSystems.getDefault().newWatchService();
-      _watcherRudisFolder = FileSystems.getDefault().newWatchService();
-      rootFolder.register(_watcherRootFolder, ENTRY_MODIFY);
-      rudisFolder.register(_watcherRudisFolder, ENTRY_MODIFY);
-    } catch (IOException e) {
-      log.error("Could not register WatchService: " + e);
-    }
-    startListening();
-  }
-
-  public void createDirectoryWatch(Project proj) {
-    _project = proj;
-    Path rootFolder = _project.getRootFolder();
-    try {
-      _watcherRootFolder = FileSystems.getDefault().newWatchService();
-      rootFolder.register(_watcherRootFolder, ENTRY_MODIFY);
-    } catch (IOException e) {
-      log.error("Could not register WatchService: " + e);
-    }
-    startListening();
-  }
-
-  public void eventLoop() throws IOException, InterruptedException {
-    for (;;) {
-
-      // watch for changes in the root folder
-      WatchKey rootKey;
-      try {
-        rootKey = _watcherRootFolder.take();
-      } catch (InterruptedException x) {
-        return;
-      }
-
-      for (WatchEvent<?> event : rootKey.pollEvents()) {
-        WatchEvent.Kind<?> kind = event.kind();
-
-        if (kind == OVERFLOW) {
-          continue;
-        }
-
-        WatchEvent<Path> ev = (WatchEvent<Path>) event;
-        Path filename = ev.context();
-
-        log.debug("Root folder change: " + filename);
-        // wait for the file being finished
-        Thread.sleep(500);
-        _project.retrieveLocRuleTreeView();
-        Platform.runLater(() -> {
-          try {
-            _project.retrieveRuleLocMap();
-          } catch (FileNotFoundException ex) {
-            log.error(ex);
-          }
-        });
-      }
-
-      rootKey.reset();
-
-      // watch for changes in the rudi folder
-      if (_watcherRudisFolder != null) {
-        WatchKey rudiKey;
-        try {
-          rudiKey = _watcherRudisFolder.take();
-        } catch (InterruptedException x) {
-          return;
-        }
-
-        for (WatchEvent<?> event : rootKey.pollEvents()) {
-          WatchEvent.Kind<?> kind = event.kind();
-
-          if (kind == OVERFLOW) {
-            continue;
-          }
-
-          WatchEvent<Path> ev = (WatchEvent<Path>) event;
-          Path filename = ev.context();
-
-          System.out.println(filename);
-        }
-
-        rudiKey.reset();
-      }
-    }
-  }
+//
+//  static Logger log = Logger.getLogger("rudiLog");
+//
+//  // the Project
+//  private volatile Project _project;
+//
+//  // the Thread
+//  private volatile Thread watchingTread;
+//
+//  public void startListening() {
+//
+//    watchingTread = new Thread() {
+//      public void run() {
+//        try {
+//          eventLoop();
+//        } catch (IOException|InterruptedException ex) {
+//          watchingTread = null;
+//        }
+//      }
+//    };
+//    watchingTread.setDaemon(true);
+//    watchingTread.setName("watchingTread");
+//    watchingTread.start();
+//    log.info("Watch has been started.");
+//  }
+//
+//  public void shutDownListener() {
+//    Thread thr = watchingTread;
+//    if (thr != null) {
+//      thr.interrupt();
+//    }
+//  }
+//
+//  private WatchService _watcherRootFolder;
+//  private WatchService _watcherRudisFolder;
+//
+//
+//  public void createProjectWatch(Project proj) {
+//    _project = proj;
+//    Path rootFolder = _project.getRootFolder();
+//    System.out.println(rootFolder);
+//    Path rudisFolder = _project.getRudisFolder();
+//    System.out.println(rudisFolder);
+//    try {
+//      _watcherRootFolder = FileSystems.getDefault().newWatchService();
+//      _watcherRudisFolder = FileSystems.getDefault().newWatchService();
+//      rootFolder.register(_watcherRootFolder, ENTRY_MODIFY);
+//      rudisFolder.register(_watcherRudisFolder, ENTRY_MODIFY);
+//    } catch (IOException e) {
+//      log.error("Could not register WatchService: " + e);
+//    }
+//    startListening();
+//  }
+//
+//  public void eventLoop() throws IOException, InterruptedException {
+//    for (;;) {
+//
+//      System.out.println("checking");
+//
+//      // watch for changes in the root folder
+//      WatchKey rootKey;
+//      try {
+//        rootKey = _watcherRootFolder.take();
+//      } catch (InterruptedException x) {
+//        return;
+//      }
+//
+//      for (WatchEvent<?> event : rootKey.pollEvents()) {
+//        WatchEvent.Kind<?> kind = event.kind();
+//
+//        if (kind == OVERFLOW) {
+//          continue;
+//        }
+//
+//        WatchEvent<Path> ev = (WatchEvent<Path>) event;
+//        Path filename = ev.context();
+//
+//        log.debug("Root folder change: " + filename);
+//        // wait for the file being finished
+//        Thread.sleep(500);
+//        _project.retrieveLocRuleTreeView();
+//        Platform.runLater(() -> {
+//          try {
+//            _project.retrieveRuleLocMap();
+//          } catch (FileNotFoundException ex) {
+//            log.error(ex);
+//          }
+//        });
+//      }
+//
+//      if (!rootKey.reset()) {
+//        break;
+//      }
+//
+//      // watch for changes in the rudi folder
+//      if (_watcherRudisFolder != null) {
+//        WatchKey rudiKey;
+//        try {
+//          rudiKey = _watcherRudisFolder.take();
+//        } catch (InterruptedException x) {
+//          return;
+//        }
+//
+//        for (WatchEvent<?> event : rootKey.pollEvents()) {
+//          WatchEvent.Kind<?> kind = event.kind();
+//
+//          if (kind == OVERFLOW) {
+//            continue;
+//          }
+//
+//          WatchEvent<Path> ev = (WatchEvent<Path>) event;
+//          Path filename = ev.context();
+//
+//          log.info("new rudi file:" + filename);
+//        }
+//
+//        if (!rudiKey.reset()) {
+//          break;
+//        }
+//      }
+//    }
+//  }
 }
