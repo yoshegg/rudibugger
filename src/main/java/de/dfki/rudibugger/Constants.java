0937146309a4cf59fb8a5a19c97b5e876721778e
Christophe Biwer <christophe.biwer@dfki.de>
2017-10-31 19:49:06 +0100

    added documentation for Constants

--- src/main/java/de/dfki/rudibugger/Constants.java
+++ src/main/java/de/dfki/rudibugger/Constants.java
@@ -12,7 +12,6 @@ package de.dfki.rudibugger;
 public class Constants {
 
   public static String PATH_TO_RUDI_FILES = "src/main/rudi/";
-
   public static String COMPILE_FILE = "compile";
   public static String RUN_FILE = "run.sh";
   public static String RULE_LOCATION_SUFFIX = "RuleLocation.yml";
@@ -21,20 +20,24 @@ public class Constants {
   public static final int OVERWRITE_CHECK_CURRENT_WINDOW = 1;
   public static final int OVERWRITE_CHECK_NEW_WINDOW = 2;
 
+  /* used to mark the rule logging state of a rule */
   public static final int STATE_NEVER = 0;
   public static final int STATE_IF_TRUE = 1;
   public static final int STATE_IF_FALSE = 2;
   public static final int STATE_ALWAYS = 3;
   public static final int STATE_PARTLY = 9;
 
+  /* used to mark and signalize the state of the rule model */
   public static final int RULE_MODEL_UNCHANGED = 0;
   public static final int RULE_MODEL_NEWLY_CREATED = 1;
   public static final int RULE_MODEL_CHANGED = 2;
   public static final int RULE_MODEL_REMOVED = 9;
 
+  /* used to signalize the opening or closing of a project */
   public static final int PROJECT_OPEN = 1;
   public static final int PROJECT_CLOSED = 0;
 
+  /* used to mark the usage state of a file in a project */
   public static final int FILE_USED = 1;
   public static final int FILE_NOT_USED = 0;
   public static final int FILE_IS_MAIN = 2;
