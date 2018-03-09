78588bcab55ffd8929020ff69ac936c84e759c2e
Christophe Biwer <christophe.biwer@dfki.de>
2018-03-09 01:13:43 +0100

    increased version number, no more beta

--- src/main/java/de/dfki/mlt/rudibugger/MainApp.java
+++ src/main/java/de/dfki/mlt/rudibugger/MainApp.java
@@ -127,7 +127,7 @@ public class MainApp extends Application {
     /* define GUI */
     Scene scene = new Scene(root);
     scene.getStylesheets().add("/styles/Styles.css");
-    stage.setTitle("Rudibugger (beta)");
+    stage.setTitle("Rudibugger");
     stage.setScene(scene);
     Image icon = new Image("file:src/main/resources/"
             + "icons/baggerschaufel_titlebar_32x32.png");
