0d64cdf5f59cb9135c45a92c3c476ad08d813727
Christophe Biwer <christophe.biwer@dfki.de>
2018-03-16 20:21:29 +0100

    visually indicate specific files being out-of-sync

--- src/main/java/de/dfki/mlt/rudibugger/FileTreeView/RudiTreeCell.java
+++ src/main/java/de/dfki/mlt/rudibugger/FileTreeView/RudiTreeCell.java
@@ -9,7 +9,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import static de.dfki.mlt.rudibugger.Constants.*;
 import java.util.HashMap;
-import java.util.Objects;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.css.PseudoClass;
 import javafx.scene.control.TreeCell;
 
@@ -34,12 +36,28 @@ public class RudiTreeCell extends TreeCell<RudiPath> {
     put(IS_FOLDER,       new Image(ICON_PATH_FILES + "folder.png"));
   }};
 
-  /** Used to visually distinguish modified files with CSS */
+  /** Used to visually distinguish modified files with CSS. */
   private final PseudoClass modifiedFileClass
           = PseudoClass.getPseudoClass("modifiedFile");
 
+  /** Used to listen to modification changes. */
+  private final ChangeListener<Boolean> modificationListener = (o, ov, nv)
+    -> pseudoClassStateChanged(modifiedFileClass, nv);
+
+  /** Used to listen to usage state changes. */
+  private final ChangeListener<Number> usageStateListener = ((o, ov, nv)
+    -> this.setGraphic(new ImageView(ICONS_RULES.get(nv.intValue()))));
+
   @Override
   protected void updateItem(RudiPath rudiPath, boolean empty) {
+
+    /* Remove old listener of the cell */
+    RudiPath oldItem = getItem();
+    if (oldItem != null) {
+      oldItem._modifiedProperty().removeListener(modificationListener);
+      oldItem._usedProperty().removeListener(usageStateListener);
+    }
+
     super.updateItem(rudiPath, empty);
 
     if (empty || rudiPath == null) {
@@ -51,24 +69,18 @@ public class RudiTreeCell extends TreeCell<RudiPath> {
 
     } else {
 
-      /* Visually indicate out of sync files */
+      /* Visually indicate out of sync files. */
       pseudoClassStateChanged(modifiedFileClass,
               rudiPath._modifiedProperty().getValue());
-//      TODO: Not working, JavaFX bug or simply not possible?
-//      rudiPath._modifiedProperty().addListener((o, ov, nv) -> {
-//        pseudoClassStateChanged(modifiedFileClass,
-//                rudiPath._modifiedProperty().getValue());
-//      });
+      rudiPath._modifiedProperty().addListener(modificationListener);
 
+      /* Set label of TreeItem. */
       setText(rudiPath.toString());
 
-      /* Define icon and listener for icon */
+      /* Define icon and listener for icon. */
       setGraphic(new ImageView(ICONS_RULES.get(
               rudiPath._usedProperty().getValue())));
-      rudiPath._usedProperty().addListener((o, oldVal, newVal) -> {
-        if (oldVal != newVal)
-          setGraphic(new ImageView(ICONS_RULES.get(newVal.intValue())));
-      });
+      rudiPath._usedProperty().addListener(usageStateListener);
 
     }
   }
