ceb301f2f4326c38d0e4cb197a82de4d0523ed00
Christophe Biwer <christophe.biwer@dfki.de>
2018-03-18 15:50:58 +0100

    own classes for syncIndicator and compileIndicator

--- src/main/java/de/dfki/mlt/rudibugger/Controller/StatusBarController.java
+++ src/main/java/de/dfki/mlt/rudibugger/Controller/StatusBarController.java
@@ -8,6 +8,8 @@ package de.dfki.mlt.rudibugger.Controller;
 import static de.dfki.mlt.rudibugger.Constants.*;
 import de.dfki.mlt.rudibugger.DataModel;
 import de.dfki.mlt.rudibugger.RuleTreeView.ImportInfoExtended;
+import de.dfki.mlt.rudibugger.StatusBar.CompileIndicator;
+import de.dfki.mlt.rudibugger.StatusBar.SyncIndicator;
 import de.dfki.mlt.rudimant.common.ErrorWarningInfo;
 import java.util.HashMap;
 import javafx.fxml.FXML;
@@ -20,6 +22,7 @@ import javafx.scene.image.ImageView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javafx.scene.image.Image;
+import javafx.scene.input.ContextMenuEvent;
 
 /**
  *
@@ -46,155 +49,23 @@ public class StatusBarController {
 
     statusBar.textProperty().bindBidirectional(model.statusBarProperty());
 
-    compileIndicator.setImage(
-      ICONS_COMPILATION_STATUS.get(COMPILATION_UNDEFINED));
-    compileIndicatorTooltip.setText("Compilation state unknown.");
-      Tooltip.install(compileIndicator, compileIndicatorTooltip);
+    /* Initialize the sync state indicator in the lower left. */
+    SyncIndicator syncIndicator = new SyncIndicator(_syncIndicator, this);
+    syncIndicator.linkListenerToProperty(_model._modifiedFilesProperty());
 
-      compileIndicator.setOnContextMenuRequested(value -> {
-        ContextMenu cm = new ContextMenu();
+    /* Initialize the compilation state indicator in the lower left. */
+    CompileIndicator compileIndicator
+      = new CompileIndicator(_compileIndicator, this);
+    compileIndicator.linkListenerToProperty(_model._compilationStateProperty());
+    compileIndicator.setContextMenu();
 
-        int counter = 0;
-        for (ErrorWarningInfo e : _model.ruleModel.errorInfos.keySet()) {
-          ImportInfoExtended item = _model.ruleModel.errorInfos.get(e);
-          String msg = "Go to error " + (++counter)
-                     + " ("
-                     + "line " + e.getLocation().getLineNumber() + ", "
-                     + "file " + item.getLabel()
-                     + ")";
-          Label label = new Label(msg);
-          CustomMenuItem errorItem = new CustomMenuItem(label);
-          Tooltip t = new Tooltip(e.getMessage());
-          Tooltip.install(label, t);
-          errorItem.setOnAction(f -> {
-            _model.openRule(item.getAbsolutePath(),
-                            e.getLocation().getLineNumber());
-          });
-          cm.getItems().add(errorItem);
-        }
-
-        if (   (! _model.ruleModel.errorInfos.isEmpty())
-            && (! _model.ruleModel.warnInfos.isEmpty())) {
-          cm.getItems().add(new SeparatorMenuItem());
-        }
-
-        counter = 0;
-        for (ErrorWarningInfo e : _model.ruleModel.warnInfos.keySet()) {
-          ImportInfoExtended item = _model.ruleModel.warnInfos.get(e);
-          String msg = "Go to warning " + (++counter)
-                     + " ("
-                     + "line " + e.getLocation().getLineNumber() + ", "
-                     + "file " + item.getLabel()
-                     + ")";
-          Label label = new Label(msg);
-          CustomMenuItem errorItem = new CustomMenuItem(label);
-          Tooltip t = new Tooltip(e.getMessage());
-          Tooltip.install(label, t);
-          errorItem.setOnAction(f -> {
-            _model.openRule(item.getAbsolutePath(),
-                            e.getLocation().getLineNumber());
-          });
-          cm.getItems().add(errorItem);
-        }
-
-        cm.show(compileIndicator, value.getScreenX(), value.getScreenY());
-
-      });
-
-    syncIndicator.setImage(
-      ICONS_SYNC_STATUS.get(FILES_SYNC_UNDEFINED));
-    syncIndicatorTooltip.setText(".rudi and compiled .java compile status unknown."
-                               + " (Probably never compiled.)");
-      Tooltip.install(syncIndicator, syncIndicatorTooltip);
-
-    /* Listen to modifications of .rudi files. */
-    _model._modifiedFilesProperty().addListener((o, ov, nv) -> {
-      String msg = "";
-
-      switch (nv.intValue()) {
-        case FILES_SYNCED:
-          msg = ".rudi files and compiled .java files up-to-date.";
-//          statusBar.textProperty().setValue(msg);
-          break;
-        case FILES_OUT_OF_SYNC:
-          msg = ".rudi files out of sync.";
-          statusBar.textProperty().setValue(msg);
-          break;
-        case FILES_SYNC_UNDEFINED:
-          msg = ".rudi and compiled .java compile status unknown."
-              + " (Probably never compiled.)";
-//          statusBar.textProperty().setValue(msg);
-          break;
-      }
-      syncIndicatorTooltip.setText(msg);
-      Tooltip.install(syncIndicator, syncIndicatorTooltip);
-      syncIndicator.setImage(ICONS_SYNC_STATUS.get(nv.intValue()));
-    });
-
-    /* Listen to the outcome of the last compilation try. */
-    _model._compilationStateProperty().addListener((o, ov, nv) -> {
-      String msg = "";
-
-      switch (nv.intValue()) {
-        case COMPILATION_PERFECT:
-          msg = "Compilation succeeded without problems.";
-          statusBar.textProperty().setValue(msg);
-          break;
-        case COMPILATION_WITH_ERRORS:
-          msg = "Compilation succeeded with errors.";
-          statusBar.textProperty().setValue(msg);
-          break;
-        case COMPILATION_WITH_WARNINGS:
-          msg = "Compilation succeeded with warnings.";
-          statusBar.textProperty().setValue(msg);
-          break;
-        case COMPILATION_FAILED:
-          msg = "Compilation failed.";
-          statusBar.textProperty().setValue(msg);
-          break;
-        case COMPILATION_UNDEFINED:
-          msg = "Compilation state unknown.";
-          break;
-      }
-      compileIndicatorTooltip.setText(msg);
-      Tooltip.install(compileIndicator, compileIndicatorTooltip);
-      compileIndicator.setImage(ICONS_COMPILATION_STATUS.get(nv.intValue()));
-    });
   }
 
-  /** Path for icons describing compilation status. */
-  private static final String ICON_COMPILATION_STATUS_PATH
-          = "file:src/main/resources/icons/compileStatus/";
+  public void setStatusBar(String text) {
+    statusBar.setText(text);
+  }
 
-  /** Map of compilation status icons. */
-  private static final HashMap<Integer, Image> ICONS_COMPILATION_STATUS
-    = new HashMap<Integer, Image>() {{
-      put(COMPILATION_PERFECT,
-        new Image(ICON_COMPILATION_STATUS_PATH + "okay.png"));
-      put(COMPILATION_WITH_ERRORS,
-        new Image(ICON_COMPILATION_STATUS_PATH + "errors.png"));
-      put(COMPILATION_WITH_WARNINGS,
-        new Image(ICON_COMPILATION_STATUS_PATH + "warnings.png"));
-      put(COMPILATION_FAILED,
-        new Image(ICON_COMPILATION_STATUS_PATH + "failed.png"));
-      put(COMPILATION_UNDEFINED,
-        new Image(ICON_COMPILATION_STATUS_PATH + "undefined.png"));
-   }};
-
-  /** Path for icons describing sync status. */
-  private static final String ICON_SYNC_STATUS_PATH
-          = "file:src/main/resources/icons/syncStatus/";
-
-  /** Map of sync status icons. */
-  private static final HashMap<Integer, Image> ICONS_SYNC_STATUS
-    = new HashMap<Integer, Image>() {{
-      put(FILES_SYNCED,
-        new Image(ICON_SYNC_STATUS_PATH + "okay.png"));
-      put(FILES_OUT_OF_SYNC,
-        new Image(ICON_SYNC_STATUS_PATH + "out.png"));
-      put(FILES_SYNC_UNDEFINED,
-        new Image(ICON_SYNC_STATUS_PATH + "undefined.png"));
-   }};
+  public DataModel getModel() { return _model; }
 
   /** StatusBar's label at the bottom. */
   @FXML
@@ -202,16 +73,10 @@ public class StatusBarController {
 
   /** Icon for sync status of .rudi and .java code. */
   @FXML
-  private ImageView syncIndicator;
-
-  /** Tooltip for syncIndicator. */
-  private final Tooltip syncIndicatorTooltip = new Tooltip();
+  private ImageView _syncIndicator;
 
   /** Icon for outcome of last compilation. */
   @FXML
-  private ImageView compileIndicator;
-
-  /** Tooltip for compileIndicator. */
-  private final Tooltip compileIndicatorTooltip = new Tooltip();
+  private ImageView _compileIndicator;
 
 }
---
+++ src/main/java/de/dfki/mlt/rudibugger/StatusBar/CompileIndicator.java
@@ -0,0 +1,163 @@
+/*
+ * Rudibugger is a debugger for .rudi code
+ * written in the context of a bachelor's thesis
+ * by Christophe Biwer (cbiwer@coli.uni-saarland.de)
+ */
+package de.dfki.mlt.rudibugger.StatusBar;
+
+import static de.dfki.mlt.rudibugger.Constants.*;
+import de.dfki.mlt.rudibugger.Controller.StatusBarController;
+import de.dfki.mlt.rudibugger.DataModel;
+import de.dfki.mlt.rudibugger.RuleTreeView.ImportInfoExtended;
+import de.dfki.mlt.rudimant.common.ErrorWarningInfo;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.event.EventHandler;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ContextMenuEvent;
+
+/**
+ * This class controls the behaviour of the compile indicator in the lower left
+ * of rudibugger. It indicates the outcome of the last compilation attempt.
+ *
+ * @author Christophe Biwer (yoshegg) christophe.biwer@dfki.de
+ */
+public class CompileIndicator {
+
+  /** The Controller. */
+  private StatusBarController _controller;
+
+  /** An icon showing the current sync state. */
+  private ImageView _indicator;
+
+  /** Shows an explanation of the current sync state. */
+  private Tooltip _tooltip;
+
+  /** Path for icons describing compilation status. */
+  private static final String ICONS_PATH
+          = "file:src/main/resources/icons/compileStatus/";
+
+  /** Map of compilation status icons. */
+  private static final HashMap<Integer, Image> ICONS
+    = new HashMap<Integer, Image>() {{
+      put(COMPILATION_PERFECT,       new Image(ICONS_PATH + "okay.png"));
+      put(COMPILATION_WITH_ERRORS,   new Image(ICONS_PATH + "errors.png"));
+      put(COMPILATION_WITH_WARNINGS, new Image(ICONS_PATH + "warnings.png"));
+      put(COMPILATION_FAILED,        new Image(ICONS_PATH + "failed.png"));
+      put(COMPILATION_UNDEFINED,     new Image(ICONS_PATH + "undefined.png"));
+   }};
+
+  /** Map of compilation status tooltip's texts. */
+  private static final HashMap<Integer, String> MESSAGES
+    = new HashMap<Integer, String>() {{
+      put(COMPILATION_PERFECT,       "Compilation succeeded without problems");
+      put(COMPILATION_WITH_ERRORS,   "Compilation succeeded with errors.");
+      put(COMPILATION_WITH_WARNINGS, "Compilation succeeded with warnings.");
+      put(COMPILATION_FAILED,        "Compilation failed.");
+      put(COMPILATION_UNDEFINED,     "Compilation state unknown.");
+    }};
+
+  /** Creates a new instance of <code>CompileIndicator</code>, creates a tooltip
+   * instance and links everything to the <code>StatusBarController</code>.
+   *
+   * @param indicator
+   * @param controller
+   */
+  public CompileIndicator(ImageView indicator, StatusBarController controller) {
+    _indicator = indicator;
+    _tooltip = new Tooltip();
+    _controller = controller;
+
+    /* Initializes the default look and behaviour if no project is loaded. */
+    _indicator.setImage(ICONS.get(COMPILATION_UNDEFINED));
+    _tooltip.setText(MESSAGES.get(COMPILATION_UNDEFINED));
+    Tooltip.install(_indicator, _tooltip);
+  }
+
+  /** Responsible for updating tooltip and icon. */
+  private final ChangeListener<Number> listener = ((cl, ov, nv) -> {
+      int val = nv.intValue();
+      String msg = MESSAGES.get(val);
+
+      if (val != COMPILATION_UNDEFINED)
+        _controller.setStatusBar(msg);
+
+      _tooltip.setText(msg);
+      Tooltip.install(_indicator, _tooltip);
+
+      _indicator.setImage(ICONS.get(val));
+  });
+
+  /**
+   * Links a given property to the listener of <code>CompileIndicator</code>.
+   *
+   * @param property
+   */
+  public void linkListenerToProperty(IntegerProperty property) {
+    property.addListener(listener);
+  }
+
+  private final EventHandler<? super ContextMenuEvent> contextMenu = (value -> {
+    ContextMenu cm = new ContextMenu();
+
+    DataModel model = _controller.getModel();
+    LinkedHashMap<ErrorWarningInfo, ImportInfoExtended> errorInfos;
+    errorInfos = model.ruleModel.errorInfos;
+    LinkedHashMap<ErrorWarningInfo, ImportInfoExtended> warnInfos;
+    warnInfos = model.ruleModel.warnInfos;
+
+    addErrorWarningInfosToContextMenu(cm, "error", errorInfos, model);
+    if ((! errorInfos.isEmpty()) && (! warnInfos.isEmpty()))
+      cm.getItems().add(new SeparatorMenuItem());
+    addErrorWarningInfosToContextMenu(cm, "warning", warnInfos, model);
+
+    cm.show(_indicator, value.getScreenX(), value.getScreenY());
+  });
+
+  /**
+   * Adds warnings or errors from the current <code>RuleModel</code> to the
+   * <code>ContextMenu</code> of the <code>CompileIndicator</code>.
+   *
+   * @param cm
+   * @param type
+   * @param data
+   * @param model
+   */
+  private void addErrorWarningInfosToContextMenu(ContextMenu cm, String type,
+    LinkedHashMap<ErrorWarningInfo, ImportInfoExtended> data, DataModel model) {
+    int counter = 0;
+    for (ErrorWarningInfo e : data.keySet()) {
+          ImportInfoExtended item = data.get(e);
+          String msg = "Go to " + type + " " + (++counter)
+                     + " ("
+                     + "line " + e.getLocation().getLineNumber() + ", "
+                     + "file " + item.getLabel()
+                     + ")";
+          Label label = new Label(msg);
+          CustomMenuItem errorItem = new CustomMenuItem(label);
+          Tooltip t = new Tooltip(e.getMessage());
+          Tooltip.install(label, t);
+          errorItem.setOnAction(f -> {
+            model.openRule(item.getAbsolutePath(),
+                            e.getLocation().getLineNumber());
+          });
+          cm.getItems().add(errorItem);
+        }
+  }
+
+
+  public void setContextMenu() {
+    _indicator.setOnContextMenuRequested(contextMenu);
+  }
+
+
+
+}
